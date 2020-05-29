448afc1fb36433095cb8aef9605d196bd7372ce8
Alvaro Camillo Neto <alvaro.camilloneto@gmail.com>
2020-05-29 00:43:51 -0300

    Criação do input com typeahead

--- byte-bank-broker/src/app/stock/stock.component.ts
+++ byte-bank-broker/src/app/stock/stock.component.ts
@@ -1,21 +1,30 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, of, merge } from 'rxjs';
 import { Stocks, Stock } from './model/stock';
 import { StockService } from './stock.service';
+import { FormControl } from '@angular/forms';
+import { filter, distinctUntilChanged, debounceTime, switchMap } from 'rxjs/operators';
+
 
 @Component({
   selector: 'app-stock',
   templateUrl: './stock.component.html',
   styleUrls: ['./stock.component.css'],
 })
-export class StockComponent implements OnInit {
-  stocks$: Observable<Stocks>;
+export class StockComponent {
+  stockInput = new FormControl();
+  stocks$: Observable<Stocks> = merge(
+    of([]),
+    this.stockInput.valueChanges
+  ).pipe(
+    filter((query) => query.length > 3),
+    distinctUntilChanged(),
+    debounceTime(333),
+    switchMap((query) => this.stockService.getStocks(query))
+  );
 
   constructor(private stockService: StockService) {}
 
-  ngOnInit(): void {
-    this.stocks$ = this.stockService.getStocks();
-  }
 
   select(stock: Stock) {
     console.log(stock);

6d9beb1eff2bfcf2f9390fcc36aafa6aee3e0cb7
Alvaro Camillo Neto <alvaro.camilloneto@gmail.com>
2020-05-29 01:17:25 -0300

    inclusao de default todas as ações

--- byte-bank-broker/src/app/stock/stock.component.ts
+++ byte-bank-broker/src/app/stock/stock.component.ts
@@ -1,10 +1,16 @@
-import { Component, OnInit } from '@angular/core';
-import { Observable, of, merge } from 'rxjs';
+import { Component } from '@angular/core';
+import { Observable, of, merge, iif } from 'rxjs';
 import { Stocks, Stock } from './model/stock';
 import { StockService } from './stock.service';
 import { FormControl } from '@angular/forms';
-import { filter, distinctUntilChanged, debounceTime, switchMap } from 'rxjs/operators';
-
+import {
+  filter,
+  distinctUntilChanged,
+  debounceTime,
+  switchMap,
+  mergeMap,
+  tap,
+} from 'rxjs/operators';
 
 @Component({
   selector: 'app-stock',
@@ -13,18 +19,21 @@ import { filter, distinctUntilChanged, debounceTime, switchMap } from 'rxjs/oper
 })
 export class StockComponent {
   stockInput = new FormControl();
-  stocks$: Observable<Stocks> = merge(
-    of([]),
-    this.stockInput.valueChanges
-  ).pipe(
-    filter((query) => query.length > 3),
+  allstocks$ = this.stockService.getStocks();
+  filterByInput$ = this.stockInput.valueChanges.pipe(
+    filter((query) => query.length > 2),
     distinctUntilChanged(),
     debounceTime(333),
     switchMap((query) => this.stockService.getStocks(query))
   );
+  stocks$ = merge(of(''), this.stockInput.valueChanges).pipe(
+    tap((value) => console.log(value)),
+    mergeMap((inputValue) =>
+      iif(() => inputValue.length, this.filterByInput$, this.allstocks$)
+    )
+  );
 
   constructor(private stockService: StockService) {}
-
 
   select(stock: Stock) {
     console.log(stock);

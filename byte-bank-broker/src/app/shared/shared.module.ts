933cf12348462518ecf2bd6c6d611a27e5d7179c
Alvaro Camillo Neto <alvaro.camilloneto@gmail.com>
2020-06-03 23:37:22 -0300

    Criação da rota da tabela do Portfolio

--- byte-bank-broker/src/app/shared/shared.module.ts
+++ byte-bank-broker/src/app/shared/shared.module.ts
@@ -4,6 +4,7 @@ import { PoModule } from '@po-ui/ng-components';
 import { MessagesModule } from '../messages/messages.module';
 import { NoDataModule } from '../no-data/no-data.module';
 import { ReactiveFormsModule } from '@angular/forms';
+import { AuthorizationModule } from '../authorization/authorization.module';
 
 @NgModule({
   imports: [
@@ -12,7 +13,14 @@ import { ReactiveFormsModule } from '@angular/forms';
     MessagesModule,
     NoDataModule,
     ReactiveFormsModule,
+    AuthorizationModule,
   ],
-  exports: [PoModule, MessagesModule, NoDataModule, ReactiveFormsModule],
+  exports: [
+    PoModule,
+    MessagesModule,
+    NoDataModule,
+    ReactiveFormsModule,
+    AuthorizationModule,
+  ],
 })
 export class SharedModule {}

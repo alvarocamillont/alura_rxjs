934e6adc8c965542b69d9fcc9d4efc60d3ef567f
Alvaro Camillo Neto <alvaro.camilloneto@gmail.com>
2020-06-03 23:48:42 -0300

    Refatoração

--- byte-bank-broker/src/app/portfolio/portfolio.module.ts
+++ byte-bank-broker/src/app/portfolio/portfolio.module.ts
@@ -2,12 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { PortfolioRoutingModule } from './portfolio-routing.module';
-import { PortfolioComponent } from './portfolio.component';
 import { SharedModule } from '../shared/shared.module';
 import { PoTemplatesModule } from '@po-ui/ng-templates';
+import { PortfolioListComponent } from './portfolio-list/portfolio-list.component';
 
 @NgModule({
-  declarations: [PortfolioComponent],
+  declarations: [PortfolioListComponent],
   imports: [
     CommonModule,
     PortfolioRoutingModule,

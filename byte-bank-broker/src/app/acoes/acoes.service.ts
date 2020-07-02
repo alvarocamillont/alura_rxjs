898478c4c40602a85d40bbff078665a1f847364c
Alvaro <alvaro.camilloneto@gmail.com>
2020-07-01 23:54:18 -0300

    Aula 1.3

--- byte-bank-broker/src/app/acoes/acoes.service.ts
+++ byte-bank-broker/src/app/acoes/acoes.service.ts
@@ -1,9 +1,15 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
+import { AcoesAPI } from './modelo/acoes';
+
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AcoesService {
+  constructor(private httpClient: HttpClient) {}
 
-  constructor() { }
+  getAcoes() {
+    return this.httpClient.get<AcoesAPI>('http://localhost:3000/acoes');
+  }
 }

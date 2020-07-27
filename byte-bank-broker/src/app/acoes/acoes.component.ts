e9cb42be7d11b50702edf75abacf2c434c96aa30
Alvaro <alvaro.camilloneto@gmail.com>
2020-07-26 22:26:16 -0300

    Operador filter

--- byte-bank-broker/src/app/acoes/acoes.component.ts
+++ byte-bank-broker/src/app/acoes/acoes.component.ts
@@ -3,8 +3,8 @@ import { FormControl } from '@angular/forms';
 
 import { AcoesService } from './acoes.service';
 import { Acoes } from './modelo/acoes';
-import { Subscription } from 'rxjs';
-import { tap, switchMap } from 'rxjs/operators';
+import { Subscription, merge } from 'rxjs';
+import { tap, switchMap, filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-acoes',
@@ -13,10 +13,20 @@ import { tap, switchMap } from 'rxjs/operators';
 })
 export class AcoesComponent {
   acoesInput = new FormControl();
-  acoes$ = this.acoesInput.valueChanges.pipe(
+
+  todasAcoes$ = this.acoesService
+    .getAcoes()
+    .pipe(tap(() => console.log('Fluxo inicial')));
+
+  filtroPeloInput$ = this.acoesInput.valueChanges.pipe(
+    tap(console.log),
+    filter((valor) => valor.length >= 3),
     tap(console.log),
     switchMap((valorDigitado) => this.acoesService.getAcoes(valorDigitado)),
     tap(console.log)
   );
+
+  acoes$ = merge(this.todasAcoes$, this.filtroPeloInput$);
+
   constructor(private acoesService: AcoesService) {}
 }

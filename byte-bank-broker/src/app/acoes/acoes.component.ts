7665991a39c200c8051e20ef2e1859bf784a2669
Alvaro <alvaro.camilloneto@gmail.com>
2020-07-26 23:03:55 -0300

    Video 4_2

--- byte-bank-broker/src/app/acoes/acoes.component.ts
+++ byte-bank-broker/src/app/acoes/acoes.component.ts
@@ -4,7 +4,15 @@ import { FormControl } from '@angular/forms';
 import { AcoesService } from './acoes.service';
 import { Acoes } from './modelo/acoes';
 import { Subscription, merge } from 'rxjs';
-import { tap, switchMap, filter } from 'rxjs/operators';
+import {
+  tap,
+  switchMap,
+  filter,
+  debounceTime,
+  distinctUntilChanged,
+} from 'rxjs/operators';
+
+const ESPERA_DIGITACAO = 300;
 
 @Component({
   selector: 'app-acoes',
@@ -19,8 +27,9 @@ export class AcoesComponent {
     .pipe(tap(() => console.log('Fluxo inicial')));
 
   filtroPeloInput$ = this.acoesInput.valueChanges.pipe(
+    debounceTime(ESPERA_DIGITACAO),
     tap(console.log),
-    filter((valor) => valor.length >= 3),
+    filter((valor) => valor.length >= 3 || !valor.length),
     tap(console.log),
     switchMap((valorDigitado) => this.acoesService.getAcoes(valorDigitado)),
     tap(console.log)
